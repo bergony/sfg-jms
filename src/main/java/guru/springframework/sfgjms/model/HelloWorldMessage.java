2ea7e8d647fecc4a745c6fcf9e59d9c51f2df6b9
Bergony <Bergony@hotmail.com>
2021-05-29 19:53:26 -0300

    Seed message. close #5

--- src/main/java/guru/springframework/sfgjms/model/HelloWorldMessage.java
+++ src/main/java/guru/springframework/sfgjms/model/HelloWorldMessage.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 @Data
 public class HelloWorldMessage implements Serializable {
 
-    static final long serialVersionUID = 6703826490277916847L;
+    static final long serialVersionUID = -6703826490277916847L;
 
     private UUID id;
     private String message;
